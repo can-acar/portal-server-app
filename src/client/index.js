17df7011f5aa70d4d06291255578188bd4401a0f
Can Acar <can.acar@windowslive.com>
2021-02-16 00:01:16 +0300

    intial commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,25 +1,45 @@
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11';
+// For IE 11 support
 import React from 'react';
-import { hydrate } from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from '../common/store/configureStore';
-import App from '../common/containers/App';
+import {hydrate} from 'react-dom';
+import {Helmet, HelmetProvider} from 'react-helmet-async';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {far} from "@fortawesome/free-regular-svg-icons";
+import App from "../app/index";
 
-const store = configureStore(window.__PRELOADED_STATE__);
+//import {Provider} from 'react-redux';
+
+
+//const store = configureStore(window.__PRELOADED_STATE__);
+const helmetContext = {
+    htmlAttributes: {lang: 'tr'}
+};
+
+library.add(fab, fas, far)
 
 hydrate(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    // <Provider store={store}>
+    <HelmetProvider context={helmetContext}>
+        <Helmet htmlAttributes={{lang: 'tr'}}>
+            <title>Razzle React Server Render</title>
+        </Helmet>
+        <App/>
+    </HelmetProvider>,
+    // </Provider>,
+    document.getElementById('root')
 );
 
-if (module.hot) {
-  module.hot.accept('../common/containers/App', () => {
-    hydrate(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-      document.getElementById('root')
-    );
-  });
+if(module.hot){
+    module.hot.accept('../app', () => {
+        hydrate(
+            // <Provider store={store}>
+            <HelmetProvider context={helmetContext}>
+                <App/>,
+            </HelmetProvider>,
+            document.getElementById('root')
+        );
+    });
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,77 +1,81 @@
-import App from '../common/containers/App';
-import { Provider } from 'react-redux';
+//import App from '../common/containers/App';
+//import {Provider} from 'react-redux';
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import configureStore from '../common/store/configureStore';
+import {Helmet} from 'react-helmet';
+//import configureStore from '../common/store/configureStore';
 import express from 'express';
-import { fetchCounter } from '../common/api/counter';
+//import {fetchCounter} from '../common/api/counter';
 import qs from 'qs';
-import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import {renderToString} from 'react-dom/server';
+import {HelmetProvider} from "react-helmet-async";
+import {StaticRouter} from 'react-router-dom';
 import serialize from 'serialize-javascript';
+import App from "../app/index";
 
+const helmetContext = {
+    htmlAttributes: {lang: 'tr'}
+};
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
 
 server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
-    fetchCounter(apiResult => {
-      // Read the counter from the request, if provided
-      const context = {};
-      const params = qs.parse(req.query);
-      const counter = parseInt(params.counter, 10) || apiResult || 0;
+    .disable('x-powered-by')
+    .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+    .get('/*', (req, res) => {
 
-      // Compile an initial state
-      const preloadedState = { counter };
+        // Read the counter from the request, if provided
+        const context = {};
+        const params = qs.parse(req.query);
+        const counter = parseInt(params.counter, 10) || 0;
 
-      // Create a new Redux store instance
-      const store = configureStore(preloadedState);
+        // Compile an initial state
+        const preloadedState = {counter};
 
-      // Render the component to a string
-      const markup = renderToString(
-        <Provider store={store}>
-           <StaticRouter
+        // Create a new Redux store instance
+        //const store = configureStore(preloadedState);
 
-               location={req.url} context={context}>
-            <App />
-          </StaticRouter>
-        </Provider>
-      );
+        // Render the component to a string
+        const markup = renderToString(
+            // <Provider store={store}>
+            <HelmetProvider context={helmetContext}>
+                <StaticRouter
+                    location={req.url} context={context}>
+                    <App/>
+                </StaticRouter>
+            </HelmetProvider>
+        );
 
-      // Grab the initial state from our Redux store
-      const finalState = store.getState();
+        // Grab the initial state from our Redux store
+        //const finalState = store.getState();
 
-      const helmet = Helmet.renderStatic();
+        const helmet = Helmet.renderStatic();
 
-      res.send(`<!doctype html>
-    <html lang="${assets.client.lang}" ${helmet.htmlAttributes.toString()}>
+        res.send(`<!doctype html>
+    <html  ${helmet.htmlAttributes.toString()}>
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
         ${helmet.title.toString()}
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         ${helmet.meta.toString()} 
         ${helmet.link.toString()} 
         ${assets.client.css
-          ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-        
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''}
+   
     </head>
     <body ${helmet.bodyAttributes.toString()}>
-        <div id="root">${markup}</div>
+        <div id="root" class="d-flex flex-column flex-root">${markup}</div>
         <script>
-          window.__PRELOADED_STATE__ = ${serialize(finalState)}
+          window.__PRELOADED_STATE__ = ${serialize({})}
         </script>
    
         ${process.env.NODE_ENV === 'production'
-        ? `<script src="${assets.client.js}" defer></script>`
-        : `<script src="${assets.client.js}" defer crossorigin></script>`}
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`}
     </body>
 </html>`);
     });
-  });
 
 export default server;
